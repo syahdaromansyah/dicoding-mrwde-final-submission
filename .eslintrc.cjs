ea1d201162af22bfa351bb77e3f2933ac7609ff0
Syahda Romansyah <syahdarom@gmail.com>
2024-02-25 00:16:37 +0700

    chore(project): initialize a project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['jsx-a11y', 'react-refresh', 'unused-imports'],
+  rules: {
+    'no-console': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'react/jsx-filename-extension': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# The _Menjadi React Web Developer Expert_ Course on Dicoding
+
+The **Become an Expert React Web Developer** course on Dicoding.
+
+## The _Membangun Aplikasi React dengan Redux_ Submission (The First Submission)
+
+The **Building React Applications with Redux** submission (the first submission).
