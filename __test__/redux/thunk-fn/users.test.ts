42bb9d0e43a34e971e4db4f1057bd621b7842473
Syahda Romansyah <syahdarom@gmail.com>
2024-04-09 06:26:33 +0700

    test: create a test of users thunk function

---
+++ __test__/redux/thunk-fn/users.test.ts
@@ -0,0 +1,77 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { fetchUsers } from '../../../src/rtk/slices/usersSlice';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { afterAll, describe, expect, test, vi } from 'vitest';
+
+const usersResponseData = {
+  data: {
+    status: 'success',
+    message: 'ok',
+    data: {
+      users: [
+        {
+          id: 'john_doe',
+          name: 'John Doe',
+          email: 'john@example.com',
+          avatar: 'https://generated-image-url.jpg',
+        },
+        {
+          id: 'jane_doe',
+          name: 'Jane Doe',
+          email: 'jane@example.com',
+          avatar: 'https://generated-image-url.jpg',
+        },
+        {
+          id: 'uchiha_saskeh',
+          name: 'Uchiha Saskeh',
+          email: 'saskeh@example.com',
+          avatar: 'https://generated-image-url.jpg',
+        },
+      ],
+    },
+  },
+};
+
+vi.mock('../../../src/network-data/network-data.ts', async (importOriginal) => {
+  const originalModules =
+    await importOriginal<
+      typeof import('../../../src/network-data/network-data.ts')
+    >();
+
+  return {
+    ...originalModules,
+    getUsers: () => Promise.resolve(usersResponseData),
+  };
+});
+
+describe('The Users Thunk Function', () => {
+  afterAll(() => {
+    vi.doUnmock('../../../src/network-data/network-data.ts');
+  });
+
+  test('should return the users response to store when fetching is fulfilled', async () => {
+    const usersStore = configureStore({
+      reducer(_, action) {
+        if (action.type === 'users/fetchUsers/fulfilled') {
+          return action.payload;
+        }
+
+        return {
+          data: {
+            users: [],
+          },
+          message: '',
+          status: '',
+        };
+      },
+    });
+
+    const dispatchResult = await usersStore.dispatch(fetchUsers());
+
+    expect(dispatchResult.type).toBe('users/fetchUsers/fulfilled');
+
+    const state = usersStore.getState();
+
+    expect(state).toEqual(usersResponseData.data);
+  });
+});
