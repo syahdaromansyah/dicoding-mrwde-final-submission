114b74934aec6850231cddaf5efeced131f152b9
Syahda Romansyah <syahdarom@gmail.com>
2024-04-09 16:32:23 +0700

    test: create a test for the UpVoteButton component

---
+++ __test__/react-components/route-components/UpVoteButton.test.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import '@testing-library/jest-dom/vitest';
+import { cleanup, screen } from '@testing-library/react';
+import React from 'react';
+import { afterEach, describe, expect, test } from 'vitest';
+import UpVoteButton from '../../../src/routes-components/UpVoteButton.tsx';
+import renderWithProvider from '../_utils/custom-redux-wrapper.tsx';
+
+const profileDummy = {
+  id: '',
+  name: '',
+  email: '',
+  avatar: '',
+};
+
+describe('The Up Vote Button Component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('Should show zero users vote', () => {
+    renderWithProvider(
+      <UpVoteButton
+        profile={profileDummy}
+        upVotesBy={[]}
+        handleUpVoteThread={() => null}
+      />,
+    );
+
+    expect(screen.getByRole('button')).toHaveTextContent('Total up votes is 0');
+  });
+
+  test('Should show some numbers of users vote', () => {
+    renderWithProvider(
+      <UpVoteButton
+        profile={profileDummy}
+        upVotesBy={['users-2', 'users-3']}
+        handleUpVoteThread={() => null}
+      />,
+    );
+
+    expect(screen.getByRole('button')).toHaveTextContent('Total up votes is 2');
+  });
+});
