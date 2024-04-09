78282b524cbde49c7603f7ae0021a8f32bd39548
Syahda Romansyah <syahdarom@gmail.com>
2024-04-09 14:05:56 +0700

    feat: create an auto-fetching of the profile thunk API

---
+++ src/hooks/useFetchProfile.ts
@@ -0,0 +1,35 @@
+import { useAppDispatch, useAppSelector } from '@/rtk/hooks';
+import {
+  fetchProfile,
+  selectProfile,
+  setStatusFetch,
+} from '@/rtk/slices/profileSlice';
+import { useEffect, useState } from 'react';
+
+const useFetchProfile = () => {
+  const [fetchOnce, setFetchOnce] = useState(false);
+
+  const dispatch = useAppDispatch();
+
+  const { status } = useAppSelector(selectProfile);
+
+  useEffect(() => {
+    if (fetchOnce === false) {
+      setFetchOnce(() => true);
+
+      dispatch(
+        setStatusFetch({
+          status: 'idle',
+        }),
+      );
+    }
+  }, [dispatch, fetchOnce]);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchProfile());
+    }
+  }, [dispatch, status]);
+};
+
+export default useFetchProfile;
