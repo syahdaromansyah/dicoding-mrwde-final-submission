ccfd438be4ae7fe9c0d73aad6312742074e81a9f
Syahda Romansyah <syahdarom@gmail.com>
2024-04-09 16:35:42 +0700

    feat: update the Comment component

--- src/routes-components/thread/Comment.tsx
+++ src/routes-components/thread/Comment.tsx
@@ -1,28 +1,23 @@
-import useProfileSlice from '@/hooks/useProfileSlice';
-import type { TCommentData, TUsersData } from '@/types/types';
-import cn from 'classnames';
+import type { TCommentData, TProfileData, TUsersData } from '@/types/types';
 import { formatDistance } from 'date-fns';
 import type { MouseEventHandler } from 'react';
-import { MdOutlineThumbDown, MdOutlineThumbUp } from 'react-icons/md';
 import MarkdownContent from '../../components/MarkdownContent';
+import DownVoteButton from '../DownVoteButton';
+import UpVoteButton from '../UpVoteButton';
 
 export default function Comment({
+  profile,
   dataUsers,
   comment,
   handleUpVoteComment,
   handleDownVoteComment,
 }: Readonly<{
+  profile: TProfileData;
   dataUsers: TUsersData;
   comment: TCommentData;
-  handleUpVoteComment: (
-    commentId: string,
-  ) => MouseEventHandler<HTMLButtonElement>;
-  handleDownVoteComment: (
-    commentId: string,
-  ) => MouseEventHandler<HTMLButtonElement>;
+  handleUpVoteComment: MouseEventHandler<HTMLButtonElement> | undefined;
+  handleDownVoteComment: MouseEventHandler<HTMLButtonElement> | undefined;
 }>) {
-  const { profile } = useProfileSlice();
-
   return (
     <div className="rounded-md p-4 dark:bg-gray-950">
       <div className="flex items-center justify-between">
@@ -63,57 +58,19 @@ export default function Comment({
 
       <div className="flex items-center gap-x-4">
         <div>
-          <p>
-            <button
-              className={cn(
-                'flex items-center justify-center gap-x-1 rounded-md px-2 py-1 transition hover:bg-indigo-300 dark:hover:bg-indigo-600',
-                {
-                  'dark:bg-indigo-600': comment.upVotesBy.find(
-                    (upVoteBy) => upVoteBy === profile.id,
-                  ),
-                },
-                {
-                  'bg-indigo-300': comment.upVotesBy.find(
-                    (upVoteBy) => upVoteBy === profile.id,
-                  ),
-                },
-              )}
-              type="button"
-              onClick={handleUpVoteComment(comment.id)}
-            >
-              <span className="inline-block text-xl">
-                <MdOutlineThumbUp />
-              </span>
-              {comment.upVotesBy.length}
-            </button>
-          </p>
+          <UpVoteButton
+            profile={profile}
+            upVotesBy={comment.upVotesBy}
+            handleUpVoteThread={handleUpVoteComment}
+          />
         </div>
 
         <div>
-          <p>
-            <button
-              className={cn(
-                'flex items-center justify-center gap-x-1 rounded-md p-1 transition hover:bg-gray-200 dark:hover:bg-gray-600',
-                {
-                  'dark:bg-gray-600': comment.downVotesBy.find(
-                    (downVoteBy) => downVoteBy === profile.id,
-                  ),
-                },
-                {
-                  'bg-gray-200': comment.downVotesBy.find(
-                    (downVoteBy) => downVoteBy === profile.id,
-                  ),
-                },
-              )}
-              type="button"
-              onClick={handleDownVoteComment(comment.id)}
-            >
-              <span className="inline-block text-xl">
-                <MdOutlineThumbDown />
-              </span>
-              {comment.downVotesBy.length}
-            </button>
-          </p>
+          <DownVoteButton
+            profile={profile}
+            downVotesBy={comment.downVotesBy}
+            handleDownVoteThread={handleDownVoteComment}
+          />
         </div>
       </div>
     </div>
