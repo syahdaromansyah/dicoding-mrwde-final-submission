0ce928167f9a5416ae63a692a0ede22943cb2ad3
Syahda Romansyah <syahdarom@gmail.com>
2024-04-09 16:40:37 +0700

    feat: update the app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import { routeTree } from './routeTree.gen.ts';
-import { store } from './rtk/store.ts';
+import { setupStore } from './rtk/store.ts';
 
 const router = createRouter({
   routeTree,
@@ -22,7 +22,7 @@ const rootElement = document.getElementById('app') as HTMLDivElement;
 if (!rootElement.innerHTML) {
   createRoot(rootElement).render(
     <React.StrictMode>
-      <ReduxProvider store={store}>
+      <ReduxProvider store={setupStore()}>
         <RouterProvider router={router} />
       </ReduxProvider>
     </React.StrictMode>,
