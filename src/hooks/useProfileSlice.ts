e55c83d67d7026b8471a9ddbad05019c4cf0f536
Syahda Romansyah <syahdarom@gmail.com>
2024-04-09 14:03:16 +0700

    feat: remove the auto-fetching of the profile thunk API

--- src/hooks/useProfileSlice.ts
+++ src/hooks/useProfileSlice.ts
@@ -1,39 +1,16 @@
-import { useAppDispatch, useAppSelector } from '@/rtk/hooks';
+import { useAppSelector } from '@/rtk/hooks';
 import {
   fetchProfile,
   selectProfile,
   setProfile,
-  setStatusFetch,
 } from '@/rtk/slices/profileSlice';
-import { useEffect, useState } from 'react';
 
 const useProfileSlice = () => {
-  const [fetchOnce, setFetchOnce] = useState(false);
-
-  const dispatch = useAppDispatch();
-
   const { profile, status } = useAppSelector(selectProfile);
-
-  useEffect(() => {
-    if (fetchOnce === false) {
-      setFetchOnce(() => true);
-
-      dispatch(
-        setStatusFetch({
-          status: 'idle',
-        }),
-      );
-    }
-  }, [dispatch, fetchOnce]);
-
-  useEffect(() => {
-    if (status === 'idle') {
-      dispatch(fetchProfile());
-    }
-  }, [dispatch, status]);
 
   return {
     profile: profile.data.user,
+    fetchProfile,
     statusProfile: status,
     setProfile,
   };
