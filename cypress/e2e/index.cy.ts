9d7f9f29fefbc1a7963196eb1a342fd1c008a4c0
Syahda Romansyah <syahdarom@gmail.com>
2024-04-17 22:08:56 +0700

    test: create E2E testing for the index route

---
+++ cypress/e2e/index.cy.ts
@@ -0,0 +1,38 @@
+/**
+ * E2E Scenario (1 Test Scenario)
+ * ~ The Index Page Test
+ *   - should show the index page content
+ */
+
+describe('The Index Page Test', () => {
+  const filterThreadsInput = '[data-cy="filter-threads-input"]';
+  const navHeaderBtn = '[data-cy="app-nav-header-menu-btn"]';
+
+  it('should show the index page content', () => {
+    cy.visit('/');
+
+    cy.get('a').contains('Forumy');
+    cy.get(navHeaderBtn).should('be.visible');
+
+    cy.get('h2').contains('Kategori popular');
+
+    cy.get('p').contains('#redux');
+    cy.get('p').contains('#intro');
+
+    cy.get('h2').contains('Diskusi tersedia');
+
+    cy.get(filterThreadsInput).should('be.visible');
+
+    cy.get('h2').contains('Hello and Welcome to My Thread!');
+
+    cy.contains(
+      'Hello there and welcome to my thread! Please introduce yourself in the comment below...',
+    );
+
+    cy.get('h2').contains('How was your experience when learning Redux?');
+
+    cy.contains(
+      'Please share your experience when learning Redux in the comment below...',
+    );
+  });
+});
