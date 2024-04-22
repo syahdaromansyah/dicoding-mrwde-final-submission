4f10dbde500ebd0d75f7570a19e0d3dfeeafe4f6
Syahda Romansyah <syahdarom@gmail.com>
2024-04-22 21:24:16 +0700

    feat: update the profile Redux slice

--- src/rtk/slices/profileSlice.ts
+++ src/rtk/slices/profileSlice.ts
@@ -4,24 +4,7 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import type { RootState } from '../store';
 
-type TProfilePayloadAction = {
-  profile: {
-    data: {
-      user: {
-        id: string;
-        name: string;
-        email: string;
-        avatar: string;
-      };
-    };
-    message: string;
-    status: string;
-  };
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
-  error: string | null | undefined;
-};
-
-const initialState: {
+export const initialState: {
   profile: TProfileResponse;
   status: 'idle' | 'loading' | 'succeeded' | 'failed';
   error: string | null | undefined;
@@ -54,21 +37,19 @@ export const profileSlice = createSlice({
   name: 'profile',
   initialState,
   reducers: {
-    setProfile(state, action: PayloadAction<TProfilePayloadAction>) {
+    resetProfile(state) {
       const currentState = state;
 
-      const { profile, status, error } = action.payload;
+      currentState.profile.data.user.id = '';
+      currentState.profile.data.user.name = '';
+      currentState.profile.data.user.email = '';
+      currentState.profile.data.user.avatar = '';
 
-      currentState.profile.data.user.id = profile.data.user.id;
-      currentState.profile.data.user.name = profile.data.user.name;
-      currentState.profile.data.user.email = profile.data.user.email;
-      currentState.profile.data.user.avatar = profile.data.user.avatar;
+      currentState.profile.message = '';
+      currentState.profile.status = '';
 
-      currentState.profile.message = profile.message;
-      currentState.profile.status = profile.status;
-
-      currentState.status = status;
-      currentState.error = error;
+      currentState.status = 'idle';
+      currentState.error = null;
     },
     setStatusFetch(state, action: PayloadAction<TSetStatusFetch>) {
       const currentState = state;
@@ -107,7 +88,7 @@ export const profileSlice = createSlice({
   },
 });
 
-export const { setProfile, setStatusFetch } = profileSlice.actions;
+export const { resetProfile, setStatusFetch } = profileSlice.actions;
 
 export const selectProfile = (state: RootState) => state.profile;
 
